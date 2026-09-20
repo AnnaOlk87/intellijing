68f11ddb9415e1d487633517e6d43963b718cd41
AnnaOlk87 <anna.e.olkova@gmail.com>
2026-09-20 19:01:22 +0300

    Next

---
+++ Fail/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "2.4.10"
+    kotlin("plugin.serialization") version "2.2.20"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+}
+
+kotlin {
+    jvmToolchain(26)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
